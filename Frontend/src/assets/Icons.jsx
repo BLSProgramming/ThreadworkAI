e5d76c8aae49ba38db6536567ced2880f825a30c
BLSProgramming <bls2.working@gmail.com>
2025-12-03 13:45:28 -0500

    S

--- Frontend/src/assets/Icons.jsx
+++ Frontend/src/assets/Icons.jsx
@@ -1,4 +1,17 @@
-import { FaEye } from "react-icons/fa";
-import { FaEyeSlash } from "react-icons/fa";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { HiLightningBolt } from "react-icons/hi";
+import { FiCheckCircle, FiTrendingUp, FiUsers, FiClock, FiCreditCard, FiLifeBuoy } from "react-icons/fi";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
-export { FaEye, FaEyeSlash };
+export { 
+  FaEye, 
+  FaEyeSlash,
+  HiLightningBolt,
+  FiCheckCircle,
+  FiTrendingUp,
+  FiUsers,
+  FiClock,
+  FiCreditCard,
+  FiLifeBuoy,
+  AiOutlineLoading3Quarters
+};
